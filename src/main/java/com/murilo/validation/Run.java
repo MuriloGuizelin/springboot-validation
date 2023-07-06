1afc372c50162e6f64219446f72dfeb0ce666eeb
Murilo Guizelin <murilomotta4@gmail.com>
2023-07-06 11:19:47 -0400

    Validação de Formulário

--- src/main/java/com/murilo/validation/Run.java
+++ src/main/java/com/murilo/validation/Run.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ValidationApplication {
+public class Run {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ValidationApplication.class, args);
+		SpringApplication.run(Run.class, args);
 	}
 
 }
